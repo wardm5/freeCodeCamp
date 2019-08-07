1c95601ac597d50f9fc57261aaf96ca52c94247c
Misha Ward <wardm5@uw.edu>
2019-08-07 11:18:41 -0700

    added extra notes for consistency like other interview pratice

--- main.java
+++ main.java
@@ -1,6 +1,9 @@
 public class main {
     public static void main(String[] args) {
         Problems problems = new Problems();
+
+        // 1.1 median
+        System.out.println("\n1.1 median");
         int[] arr1 = {1,3,5};
         int[] arr2 = {2,4,6};
         System.out.println(problems.median(arr1, arr2));  // should return 3.5
