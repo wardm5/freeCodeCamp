be83b46fae986db7e8bb47a6dd366bd84539db4d
Misha Ward <wardm5@uw.edu>
2019-08-08 10:46:54 -0700

    added findMissing method

--- Problems.java
+++ Problems.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Problems {
     public double median(int[] arr1, int[] arr2) {
         if (arr1.length >= arr2.length)
@@ -30,5 +31,54 @@ public class Problems {
             return (double) (temp[(temp.length / 2) - 1] + temp[(temp.length / 2)]) / 2;
         else
             return temp[temp.length / 2];
+    }
+
+    // public int closestToTarget(int[] arr1, int[] arr2, int target) {
+    //     Arrays.sort(arr1);
+    //     Arrays.sort(arr2);
+    //     int i = 0;
+    //     int j = arr1.length - 1;
+    //     int a = 0;
+    //     int b = 0;
+    //     int val = -0;
+    //     while (i >= 0 && i < arr2.length && j >= 0) {
+    //         if (arr1[i] * arr2[j] == target) {
+    //             return target;
+    //         } else if (arr1[i] * arr2[j] <target) {
+    //             i++;
+    //
+    //             a = arr1[i];
+    //             b = arr2[j];
+    //             if (Math.abs(target - a * b) < val) {
+    //                 val = a * b;
+    //             }
+    //             System.out.println(a*b);
+    //         } else {
+    //
+    //             j--;
+    //             a = arr1[i];
+    //             b = arr2[j];
+    //             System.out.println(a*b);
+    //         }
+    //     }
+    //     return a * b;
+    // }
+    public int findMissing1(int[] arr1, int[] arr2) {
+        int sum1 = -1;
+        int sum2 = -1;
+        for (int i = 0; i < arr1.length; i++) {
+            if (i < arr2.length)
+                sum2 += arr2[i];
+            sum1 += arr1[i];
+        }
+        return sum1-sum2;
+    }
+    public int findMissing2(int[] arr1, int[] arr2) {
+        // int sum1 = -1;
+        int sum = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            sum ^= arr2[i];
+        }
+        return sum;
     }
 }
--- main.java
+++ main.java
@@ -3,6 +3,10 @@ public class main {
         Problems problems = new Problems();
 
         // 1.1 median
+        // ASK:
+        // 1) if the two arrays are even or the same length
+        // 2) how to deal with two median values, maybe return two elements or average
+        // 3) confirm if arrays of intergers... if okay to return double
         System.out.println("\n1.1 median");
         int[] arr1 = {1,3,5};
         int[] arr2 = {2,4,6};
@@ -11,5 +15,14 @@ public class main {
         int[] arr3 = {1,3,5};
         int[] arr4 = {2,4,6,7};
         System.out.println(problems.median(arr3, arr4));  // should return 3.5
+
+        // int[] arr5 = {-2, 5, 2, 6 ,4 ,7};
+        // int[] arr6 = {9, -6, 2, 7, 5, 3};
+        // System.out.println(problems.closestToTarget(arr5, arr6, 1));
+
+        System.out.println("\n1.1 findMissing");
+        int[] arr7 = {1,4,5,9,3};
+        int[] arr8 = {1,9,4,5};
+        System.out.println(problems.findMissing1(arr7, arr8));  // should return 3
     }
 }
